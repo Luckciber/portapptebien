3d3c3775cda1254594a802e9296fbfbe62f9c8a8
MATIAS V <matiasvillarroelg@hotmail.com>
2025-05-03 14:42:55 -0400

    consulta de curso

--- querys/cursos.sql
+++ querys/cursos.sql
@@ -1,9 +1,54 @@
-INSERT INTO `curso` (`descripcion`) VALUES
-('Primero Básico'),
-('Segundo Básico'),
-('Tercero Básico'),
-('Cuarto Básico'),
-('Quinto Básico'),
-('Sexto Básico'),
-('Séptimo Básico'),
-('Octavo Básico');
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 03-05-2025 a las 18:41:38
+-- Versión del servidor: 9.1.0
+-- Versión de PHP: 8.3.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `portapptebien`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cursos`
+--
+
+DROP TABLE IF EXISTS `cursos`;
+CREATE TABLE IF NOT EXISTS `cursos` (
+  `id_cursos` int NOT NULL,
+  `año` varchar(10) NOT NULL,
+  PRIMARY KEY (`id_cursos`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `cursos`
+--
+
+INSERT INTO `cursos` (`id_cursos`, `año`) VALUES
+(1, '1°basico'),
+(2, '2°basico'),
+(3, '3°basico'),
+(4, '4°basico'),
+(5, '5°basico'),
+(6, '6°basico'),
+(7, '7°basico'),
+(8, '8°basico');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
