189c8dec45ba8a055b4384e1dc84f030c1ccb086
Sthefano <sthefano.campos1@gmail.com>
2025-05-14 22:13:56 -0400

    ver citaciones hecho como corresponde y otras modificaciones en citaciones.php

--- sistema/DAO/citacionesDAO.php
+++ sistema/DAO/citacionesDAO.php
@@ -6,11 +6,11 @@ class CitacionesDao {
         $this->pdo = $pdo;
     }
 
-    public function agregarCitacion($id_alumno, $id_usuario, $fecha_creacion, $motivo, $fecha_citacion) {
-        $sql = "INSERT INTO citaciones (rut_alumno, rut_apoderado ,id_usuario, fecha_creacion, motivo, fecha_citacion) VALUES (:rut_alumno, :rut_apoderado, :id_usuario, :fecha_creacion, :motivo, :fecha_citacion)";
+    public function agregarCitacion($fecha_creacion,$rut_alumno, $id_usuario, $fecha_citacion,$motivo ) {
+        $sql = "INSERT INTO citaciones (rut_alumno, id_usuario, fecha_creacion, motivo, fecha_citacion) VALUES (:rut_alumno, :id_usuario, :fecha_creacion, :motivo, :fecha_citacion)";
         try {
             $stmt = $this->pdo->prepare($sql);
-            $stmt->bindParam(':rut_alumno', $id_alumno);
+            $stmt->bindParam(':rut_alumno', $rut_alumno);
             $stmt->bindParam(':id_usuario', $id_usuario);
             $stmt->bindParam(':fecha_creacion', $fecha_creacion);
             $stmt->bindParam(':motivo', $motivo);
@@ -67,11 +67,14 @@ class CitacionesDao {
             return false;
         }
     }
-    public function actualizarCitacion($id_citacion, $motivo, $fecha_citacion) {
-        $sql = "UPDATE citaciones SET motivo = :motivo, fecha_citacion = :fecha_citacion WHERE id_citacion = :id_citacion";
+    public function actualizarCitacion($id_citacion, $rut_alumno, $id_usuario, $fecha_creacion, $motivo, $fecha_citacion) {
+        $sql = "UPDATE citaciones SET rut_alumno = :rut_alumno, id_usuario = :id_usuario, fecha_creacion = :fecha_creacion, motivo = :motivo, fecha_citacion = :fecha_citacion WHERE id_citacion = :id_citacion";
         try {
             $stmt = $this->pdo->prepare($sql);
             $stmt->bindParam(':id_citacion', $id_citacion);
+            $stmt->bindParam(':rut_alumno', $rut_alumno);
+            $stmt->bindParam(':id_usuario', $id_usuario);
+            $stmt->bindParam(':fecha_creacion', $fecha_creacion);
             $stmt->bindParam(':motivo', $motivo);
             $stmt->bindParam(':fecha_citacion', $fecha_citacion);
             return $stmt->execute();
